a0d7b3ecd8a9aded31ac1fd516af0cd676b60ca8
Joao Vitor Paulino Martins <paulino.joaovitor@yahoo.com.br>
2023-11-14 12:50:07 -0300

    upgrading fluent validation version

--- src/Output.FluentValidation/OutputBaseExtensions.cs
+++ src/Output.FluentValidation/OutputBaseExtensions.cs
@@ -10,7 +10,7 @@ public static class OutputBaseExtensions
     public static void AddValidationResult<T>(this Output<T> output, ValidationResult validationResult)
     {
         if (validationResult is null)
-            throw new ValidationResultNullException(OutputConstants.ValidationResultNullMessage);
+            throw new ValidationResultNullException(message: OutputConstants.ValidationResultNullMessage);
         
         var errors = validationResult.Errors.Select<ValidationFailure, string>(e => $"{e.PropertyName} => {e.ErrorMessage}").ToList<string>();
         
